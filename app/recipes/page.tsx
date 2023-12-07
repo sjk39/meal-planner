2f9daf7213e58450f305d90af4d66405f765b607
sjk39 <34311263+sjk39@users.noreply.github.com>
2023-12-07 22:58:00 +0000

    headings Meal type headings

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -1,68 +1,54 @@
 "use client";
-import Link from "next/link";
+import React, { useState } from "react";
 import Layout from "../components/layout";
 import recipes from "../../server/recipes.json";
-import { useState } from "react";
 
-// type Recipe = {
-//   id: number;
-//   name: string;
-//   ingredients: string[];
-// }
-
-// type RecipesPageProps = {
-//   recipes: Recipe[];
-// }
-
-const recipesDefault = recipes
-  ? recipes
-  : [
-      {
-        id: 1,
-        name: "Chocolate Cake",
-        ingredients: ["milk", "eggs", "flour", "chocolate"],
-        mealType: "dinner",
-      },
-      { id: 2, name: "Pasta Carbonara", ingredients: [], mealType: "dinner" },
-      // ... other recipes
-    ];
-
-const RecipesPage: React.FC<any> = ({ recipes = recipesDefault }) => {
+const RecipesPage = () => {
   const [visibleRecipeId, setVisibleRecipeId] = useState<number | null>(null);
 
   const toggleIngredients = (id: number) => {
     if (visibleRecipeId === id) {
-      setVisibleRecipeId(null); // Hide if the same recipe is clicked again
+      setVisibleRecipeId(null);
     } else {
-      setVisibleRecipeId(id); // Show new recipe's ingredients
+      setVisibleRecipeId(id);
     }
+  };
+
+  const renderRecipesByMealType = (mealType: string) => {
+    return recipes
+      .filter(recipe => recipe.mealType === mealType)
+      .map(recipe => (
+        <li key={recipe.id} className="mb-2">
+          <div
+            onClick={() => toggleIngredients(recipe.id)}
+            className="group block rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 cursor-pointer"
+          >
+            <h2 className="text-xl font-semibold">{recipe.name}</h2>
+            {visibleRecipeId === recipe.id && (
+              <ul className="pl-4">
+                {recipe.ingredients.map((ingredient, index) => (
+                  <li key={index}>{ingredient}</li>
+                ))}
+              </ul>
+            )}
+          </div>
+        </li>
+      ));
   };
 
   return (
     <Layout>
       <div className="container mx-auto px-4">
         <h1 className="text-2xl font-bold mb-4">Recipes List</h1>
-        <ul className="list-none">
-          {recipes.map((recipe: any) => (
-            <li key={recipe.id} className="mb-2">
-              <div
-                onClick={() => toggleIngredients(recipe.id)}
-                className="group block rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 cursor-pointer"
-              >
-                <h2 className="text-xl font-semibold">{recipe.name}</h2>
-                {visibleRecipeId === recipe.id && (
-                  <ul className="pl-4">
-                    {recipe.ingredients.map(
-                      (ingredient: string, index: number) => (
-                        <li key={index}>{ingredient}</li>
-                      )
-                    )}
-                  </ul>
-                )}
-              </div>
-            </li>
-          ))}
-        </ul>
+
+        <h2 className="text-xl font-bold my-3">Breakfast</h2>
+        <ul className="list-none">{renderRecipesByMealType("breakfast")}</ul>
+
+        <h2 className="text-xl font-bold my-3">Lunch</h2>
+        <ul className="list-none">{renderRecipesByMealType("lunch")}</ul>
+
+        <h2 className="text-xl font-bold my-3">Dinner</h2>
+        <ul className="list-none">{renderRecipesByMealType("dinner")}</ul>
       </div>
     </Layout>
   );
