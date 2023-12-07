1db9d00a3266a1f9a7ec7ad63c75ed9b886baf8b
sjk39 <34311263+sjk39@users.noreply.github.com>
2023-12-07 22:32:53 +0000

    add-recipes add recipe functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Layout from "../components/layout";
+import Layout from "./components/layout";
 
 export default function Home() {
   return (
@@ -37,14 +37,13 @@ export default function Home() {
               </span>
             </h2>
             <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              See current recipes and add/edit/remove them.
+              See current recipes and ingredients.
             </p>
           </a>
 
           <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="/shopping"
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
             rel="noopener noreferrer"
           >
             <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -61,7 +60,7 @@ export default function Home() {
           </a>
 
           <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://www.nhs.uk/live-well/eat-well/"
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             target="_blank"
             rel="noopener noreferrer"
--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -1,5 +1,8 @@
+"use client";
 import Link from "next/link";
-import Layout from "../../components/layout";
+import Layout from "../components/layout";
+import recipes from "../../server/recipes.json";
+import { useState } from "react";
 
 // type Recipe = {
 //   id: number;
@@ -11,19 +14,30 @@ import Layout from "../../components/layout";
 //   recipes: Recipe[];
 // }
 
-const recipesDefault = [
-  {
-    id: 1,
-    name: "Chocolate Cake",
-    ingredients: ["milk", "eggs", "flour", "chocolate"],
-  },
-  { id: 2, name: "Pasta Carbonara", ingredients: [] },
-  // ... other recipes
-];
+const recipesDefault = recipes
+  ? recipes
+  : [
+      {
+        id: 1,
+        name: "Chocolate Cake",
+        ingredients: ["milk", "eggs", "flour", "chocolate"],
+        mealType: "dinner",
+      },
+      { id: 2, name: "Pasta Carbonara", ingredients: [], mealType: "dinner" },
+      // ... other recipes
+    ];
 
-const RecipesPage: React.FC<any> = ({
-  recipes = recipesDefault,
-}) => {
+const RecipesPage: React.FC<any> = ({ recipes = recipesDefault }) => {
+  const [visibleRecipeId, setVisibleRecipeId] = useState<number | null>(null);
+
+  const toggleIngredients = (id: number) => {
+    if (visibleRecipeId === id) {
+      setVisibleRecipeId(null); // Hide if the same recipe is clicked again
+    } else {
+      setVisibleRecipeId(id); // Show new recipe's ingredients
+    }
+  };
+
   return (
     <Layout>
       <div className="container mx-auto px-4">
@@ -31,11 +45,21 @@ const RecipesPage: React.FC<any> = ({
         <ul className="list-none">
           {recipes.map((recipe: any) => (
             <li key={recipe.id} className="mb-2">
-              <Link href={`/recipes/${recipe.id}`} passHref>
-                <div className="group block rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-                  <h2 className="text-xl font-semibold">{recipe.name}</h2>
-                </div>
-              </Link>
+              <div
+                onClick={() => toggleIngredients(recipe.id)}
+                className="group block rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 cursor-pointer"
+              >
+                <h2 className="text-xl font-semibold">{recipe.name}</h2>
+                {visibleRecipeId === recipe.id && (
+                  <ul className="pl-4">
+                    {recipe.ingredients.map(
+                      (ingredient: string, index: number) => (
+                        <li key={index}>{ingredient}</li>
+                      )
+                    )}
+                  </ul>
+                )}
+              </div>
             </li>
           ))}
         </ul>
