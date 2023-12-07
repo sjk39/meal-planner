1d192ad45707d9f827c602b543d5deda2d444a93
sjk39 <34311263+sjk39@users.noreply.github.com>
2023-12-07 18:05:14 +0000

    wip-edit-homepage homepage and recipe page (#1)
    * wip-edit-homepage homepage and recipe page
    
    * wip-edit-homepage typing change to allow build
    
    * wip-edit-homepage allow build

---
+++ app/recipes/page.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import Layout from "../../components/layout";
+
+// type Recipe = {
+//   id: number;
+//   name: string;
+//   ingredients: string[];
+// }
+
+// type RecipesPageProps = {
+//   recipes: Recipe[];
+// }
+
+const recipesDefault = [
+  {
+    id: 1,
+    name: "Chocolate Cake",
+    ingredients: ["milk", "eggs", "flour", "chocolate"],
+  },
+  { id: 2, name: "Pasta Carbonara", ingredients: [] },
+  // ... other recipes
+];
+
+const RecipesPage: React.FC<any> = ({
+  recipes = recipesDefault,
+}) => {
+  return (
+    <Layout>
+      <div className="container mx-auto px-4">
+        <h1 className="text-2xl font-bold mb-4">Recipes List</h1>
+        <ul className="list-none">
+          {recipes.map((recipe: any) => (
+            <li key={recipe.id} className="mb-2">
+              <Link href={`/recipes/${recipe.id}`} passHref>
+                <div className="group block rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+                  <h2 className="text-xl font-semibold">{recipe.name}</h2>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Layout>
+  );
+};
+
+export default RecipesPage;
